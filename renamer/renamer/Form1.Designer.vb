63048143b11726f642c933c67a81fdc0d1925c40
npetrie3 <npetrie3@gmail.com>
2016-05-22 00:12:18 -0400

    Add files via upload

---
+++ renamer/renamer/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.nudBegin = New System.Windows.Forms.NumericUpDown()
+        Me.nudEnd = New System.Windows.Forms.NumericUpDown()
+        Me.beginning = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtExt = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.btnRename = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        CType(Me.nudBegin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudEnd, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'nudBegin
+        '
+        Me.nudBegin.Location = New System.Drawing.Point(125, 14)
+        Me.nudBegin.Name = "nudBegin"
+        Me.nudBegin.Size = New System.Drawing.Size(49, 20)
+        Me.nudBegin.TabIndex = 0
+        '
+        'nudEnd
+        '
+        Me.nudEnd.Location = New System.Drawing.Point(125, 39)
+        Me.nudEnd.Name = "nudEnd"
+        Me.nudEnd.Size = New System.Drawing.Size(49, 20)
+        Me.nudEnd.TabIndex = 1
+        '
+        'beginning
+        '
+        Me.beginning.AutoSize = True
+        Me.beginning.Location = New System.Drawing.Point(6, 16)
+        Me.beginning.Name = "beginning"
+        Me.beginning.Size = New System.Drawing.Size(116, 13)
+        Me.beginning.TabIndex = 2
+        Me.beginning.Text = "Beginning File Number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Ending File Number:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtExt)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.beginning)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.nudBegin)
+        Me.GroupBox1.Controls.Add(Me.nudEnd)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 119)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "File Information"
+        '
+        'txtExt
+        '
+        Me.txtExt.Location = New System.Drawing.Point(125, 65)
+        Me.txtExt.Name = "txtExt"
+        Me.txtExt.Size = New System.Drawing.Size(49, 20)
+        Me.txtExt.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "File Extension:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtPath)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 13)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "File Location"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 52)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 26)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Enter the path of the file directory.  " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Example: C:\Example\"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Path:"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(44, 19)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(150, 20)
+        Me.txtPath.TabIndex = 0
+        '
+        'btnRename
+        '
+        Me.btnRename.Location = New System.Drawing.Point(242, 90)
+        Me.btnRename.Name = "btnRename"
+        Me.btnRename.Size = New System.Drawing.Size(102, 23)
+        Me.btnRename.TabIndex = 6
+        Me.btnRename.Text = "Rename Files"
+        Me.btnRename.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(242, 120)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(102, 23)
+        Me.btnReset.TabIndex = 7
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 231)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnRename)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "File Renamer"
+        CType(Me.nudBegin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudEnd, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents nudBegin As NumericUpDown
+    Friend WithEvents nudEnd As NumericUpDown
+    Friend WithEvents beginning As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtExt As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents btnRename As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ renamer/renamer/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnRename_Click(sender As Object, e As EventArgs) Handles btnRename.Click
+        Dim beginning As Integer
+        Dim ending As Integer
+        Dim extension As String
+        Dim path As String
+
+
+        beginning = nudBegin.Value
+        ending = nudEnd.Value
+        extension = txtExt.Text
+        path = txtPath.Text
+
+        Try
+            Rename(beginning, ending, extension, path)
+        Catch ex As Exception
+            MsgBox("There was an error renaming the files, please check all values and try again.", MsgBoxStyle.Critical, "Error")
+        End Try
+
+
+    End Sub
+
+    Private Sub Rename(ByVal beginning, ByVal ending, ByVal extension, ByVal path)
+
+        For i As Integer = beginning To ending
+            My.Computer.FileSystem.RenameFile(path + CStr(i) + extension, "0" + CStr(i) + extension)
+        Next
+
+        MsgBox("Files have successfully been renamed.", MsgBoxStyle.OkOnly, "Success!")
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        nudBegin.Value = 0
+        nudEnd.Value = 0
+        txtExt.Text = ""
+        txtPath.Text = ""
+    End Sub
+End Class
